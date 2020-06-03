d13ca94fac58149c3f0d209222f1be194cad3370
Julian Nash <julian.nash@bramfitt-tech-labs.com>
2020-06-03 14:30:25 +0100

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cwe",
-    version="1.0",
+    version="1.1",
     packages=setuptools.find_packages(),
     author="Julian Nash",
     author_email="julianjamesnash@gmail.com",
