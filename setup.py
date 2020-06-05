1462e2730ac8c898a6fa8dd75208626eaffef8e7
Julian Nash <julian.nash@bramfitt-tech-labs.com>
2020-06-05 16:04:47 +0100

    added requests to setup.py install_requires

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     author="Julian Nash",
     include_package_data=True,
+    install_requires=["requests"],
     author_email="julianjamesnash@gmail.com",
     description="Common weakness enumeration wrapper for Python",
     long_description=long_description,
